0c88c92532f89cead1a8929fef95c8aa24a47627
Arzamastsev Dmitry <motok1996@gmail.com>
2022-01-28 11:07:48 +0300

    initial commit

---
+++ stations/views.py
@@ -0,0 +1,25 @@
+import csv
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from pagination import settings
+
+
+def index(request):
+    return redirect(reverse('bus_stations'))
+
+
+def bus_stations(request):
+    page_number = int(request.GET.get('page', 1))
+    result = []
+    with open(settings.BUS_STATION_CSV, newline='', encoding='utf-8') as file:
+        data = csv.DictReader(file)
+        for row in data:
+            result.append(row)
+
+    paginator = Paginator(result, 10)
+    page = paginator.get_page(page_number)
+    context = {
+        'bus_stations': page,
+    }
+    return render(request, 'stations/index.html', context)
