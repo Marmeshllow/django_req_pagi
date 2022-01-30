e6f78919c3e7315ed6ffac677c96d092f9fd584d
Arzamastsev Dmitry <motok1996@gmail.com>
2022-01-30 20:15:53 +0300

    v1.1

--- stations/views.py
+++ stations/views.py
@@ -5,21 +5,23 @@ from django.urls import reverse
 from pagination import settings
 
 
+result = []
+with open(settings.BUS_STATION_CSV, newline='', encoding='utf-8') as file:
+    data = csv.DictReader(file)
+    for row in data:
+        result.append(row)
+
+
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
 def bus_stations(request):
     page_number = int(request.GET.get('page', 1))
-    result = []
-    with open(settings.BUS_STATION_CSV, newline='', encoding='utf-8') as file:
-        data = csv.DictReader(file)
-        for row in data:
-            result.append(row)
-
     paginator = Paginator(result, 10)
     page = paginator.get_page(page_number)
     context = {
-        'bus_stations': page,
+        'bus_stations': page.object_list,
+        'page': page,
     }
     return render(request, 'stations/index.html', context)
